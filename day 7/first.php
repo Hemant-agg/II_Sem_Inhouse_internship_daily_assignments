f353a891a82124dfb5f924a5b339f5796e0d19f9
Hemant Aggarwal <hemantgarg25006@gmail.com>
2026-07-12 02:47:12 +0530

    Add files via upload

---
+++ day 7/first.php
@@ -0,0 +1,11 @@
+<?php
+//echo "hello world";
+//phpinfo();
+//echo$_SARVER['REMOTE_ADDR'];
+//echo"nov 22,2006 is on a", date("1" ,mktime(0,0,7,1,2000)) ."\n";
+$name = "Nandini";
+echo $name;
+
+$Student =["Rahul","Aman"];
+print_r($Student);
+?>
